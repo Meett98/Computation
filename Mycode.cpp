4006ebcf640c27ba725f222df0b3342f9f322967
Meet <prajapatimeet908@gmail.com>
2023-08-02 17:45:25 +0530

    First Commit

---
+++ Mycode.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello There!!\nMeet here!\n";
+
+
+
+    int a = 10;
+    int b = 20;
+
+    int sum = a + b;
+
+    int sub =  b - a;
+
+    int mul = a * b;
+
+    int div = b / a;
+
+
+    cout<<"Sum : "<<sum<<"\n";
+    cout<<"Sub : "<<sub<<"\n";
+    cout<<"Mul : "<<mul<<"\n";
+    cout<<"Div : "<<div<<"\n";
+
+
+    
+}
