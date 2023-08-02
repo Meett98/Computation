a96c498dd9f4edf215f46bacdc0779f69e704b39
Meet <prajapatimeet908@gmail.com>
2023-08-02 17:48:57 +0530

    changes in the Arithmetic cpp code

--- Mycode.cpp
+++ Mycode.cpp
@@ -12,12 +12,16 @@ int main()
     int a = 10;
     int b = 20;
 
+    //this is Summation operation
     int sum = a + b;
 
+    //this is Subtract operation
     int sub =  b - a;
 
+    //this is Multiply operation
     int mul = a * b;
 
+    //this is Division operation
     int div = b / a;
 
 
